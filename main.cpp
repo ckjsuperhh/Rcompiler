ccc21cf1e55ed33057c3949d88bc35cec29e95be
ckjsuperhh <846230484@qq.com>
2025-12-18 14:35:44 +0800

    again reconstruct the docker environment

--- main.cpp
+++ main.cpp
@@ -14,7 +14,7 @@ int main() {
     // std::cerr<<input<<std::endl;
     std::string input((std::istreambuf_iterator(std::cin)),
                      std::istreambuf_iterator<char>());
-    // try {
+    try {
         Tokenizer t(input);
         auto v=t.tokenize();
         // t.ShowOutput(v);
@@ -23,29 +23,29 @@ int main() {
         if (!root) {
             std::cerr<<"unable to parse the file"<<std::endl;
         }
-        // std::vector<std::string> tree1 = root->showTree(0,true);
-        // // 逐行打印
+        std::vector<std::string> tree1 = root->showTree(0,true);
+        // 逐行打印
         // for (const std::string& line : tree1) {
         //     std::cout << line << std::endl;
         // }
         SemanticCheck sc;
         sc.Analyze(root.get());
-        // std::vector<std::string> tree2 = root->showTree(0,true);
-        // // 逐行打印
+        std::vector<std::string> tree2 = root->showTree(0,true);
+        // 逐行打印
         // for (const std::string& line : tree2) {
         //     std::cout << line << std::endl;
         // }
-        // IRgen ir;
+        IRgen ir;
         // for (auto ve=ir.generateIr(root.get()); const auto& vv:ve) {
         //     std::cout << vv << std::endl;
         // }
-    // }
-    // catch (const std::exception& e) {
-    //     std::cerr << "Error: " << e.what() << std::endl;
-    //     exit(-1);
-    // }catch (...) {  // 再捕获所有其他异常
-    //     std::cerr << "Unknown error occurred" << std::endl;
-    //     exit(-1);
-    // }
+    }
+    catch (const std::exception& e) {
+        std::cerr << "Error: " << e.what() << std::endl;
+        exit(-1);
+    }catch (...) {  // 再捕获所有其他异常
+        std::cerr << "Unknown error occurred" << std::endl;
+        exit(-1);
+    }
     return 0;
 }
